e7ce872e2bb425d930c4a4860d2832ddce7c3756
Gaurav Manjhi <Gauravmanjhi09@gmail.com>
2026-06-27 21:37:01 +0530

    streamlit setup + homescreen

---
+++ src/components/footer.py
@@ -0,0 +1,23 @@
+# import streamlit as st
+
+# def footer_home():
+#     logo_url = "https://i.ibb.co/4r5X1FY/apnacollege.png"
+
+#     st.markdown(f"""
+#         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center;">
+#             <p style="font-weight:bold; color:white;">Created by</p>
+#             <img src='{logo_url}' style='max-height:25px;' />
+#         </div>
+#     """, unsafe_allow_html=True)
+
+import streamlit as st
+
+def footer_home():
+    st.markdown("""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center;">
+            <p style="font-weight:bold; color:white;">Created by Gaurav Manjhi</p>
+        </div>
+    """, unsafe_allow_html=True)
+
+footer_home()
+
---
+++ src/screen/home_screen.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from src.components.header import header_home
+from src.components.footer import footer_home
+from src.ui.style_based_layout import style_based_layout,style_background_home
+
+def home_screen():
+   
+
+    style_background_home()
+    style_based_layout()
+    header_home()
+
+    col1, col2 = st.columns(2, gap = 'large')
+
+    with col1:
+        st.header("I'm Student")
+        st.image("https://i.ibb.co/844D9Lrt/mascot-student.png",width=120)
+        if st.button('Student Portal',type="primary",icon=':material/arrow_outward:',icon_position="right"):
+            st.session_state['Login_type'] = 'students'
+            st.rerun()
+
+    with col2:
+        st.header("I'm Teacher")
+        st.image("https://i.ibb.co/CsmQQV6X/mascot-prof.png",width=135)
+        if st.button('Teacher Portal',type="primary",icon=':material/arrow_outward:',icon_position="right"):
+            st.session_state['Login_type'] = 'teacher'
+            st.rerun()
+
+    footer_home()
