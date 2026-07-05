e6d4376cea1d9dcb7a5cc61be7687292bd126162
Gaurav Manjhi <Gauravmanjhi09@gmail.com>
2026-07-05 19:10:31 +0530

    fix student login_type typo and update gitignore

--- src/screen/home_screen.py
+++ src/screen/home_screen.py
@@ -16,7 +16,7 @@ def home_screen():
         st.header("I'm Student")
         st.image("https://i.ibb.co/844D9Lrt/mascot-student.png",width=120)
         if st.button('Student Portal',type="primary",icon=':material/arrow_outward:',icon_position="right"):
-            st.session_state['login_type'] = 'students'
+            st.session_state['login_type'] = 'student'
             st.rerun()
 
     with col2:
